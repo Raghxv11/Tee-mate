10ef8af49cae87b1203b30436d53b47023b97ea3
Raghxv11 <raghavbansal121@gmail.com>
2023-07-27 20:50:40 +0530

    added

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker"
+import AIPicker from "./AIPicker";
+import Tab from "./Tab";
+
+export { CustomButton, AIPicker, ColorPicker, Tab, FilePicker };
